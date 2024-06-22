6b92bdabe5472bb56e800b54086c2a878de2f704
Karthik Ch <saipavan39dh@gmail.com>
2024-06-23 00:10:55 +0530

    managed authmiddlewareteacher for bookmodel and bookroutes

--- Backend/routes/bookRoutes.js
+++ Backend/routes/bookRoutes.js
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import { createBook, getaBook, getallBooks, returnaBook, updateBook } from "../controllers/bookController.js";
 
+import { authMiddleWareAdmin } from "../middleware/roleManage.js";
 
 
 const router = Router()
 
-router.route('/createbook').post(createBook)
+router.route('/createbook').post(authMiddleWareAdmin,createBook)
 router.route('/getallbooks').get(getallBooks)
 router.route('/getabook').get(getaBook)
-router.route('/updatebook').put(updateBook)
+router.route('/updatebook').put(authMiddleWareAdmin,updateBook)
 router.route('/returnabook').get(returnaBook)
 
 
