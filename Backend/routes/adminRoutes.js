6da2308f28206604644f2e738d7f7784aeb7748a
Suraj Kumar <surajkumar.ayanokoji.7@gmail.com>
2024-06-22 20:50:53 +0530

    Add Books Routing links

--- Backend/routes/adminRoutes.js
+++ Backend/routes/adminRoutes.js
@@ -1,15 +1,19 @@
 import { Router } from "express";
 import { createAdmin, getaAdmin, getallAdmins, getallIssues, triggerblockadmin, updateAdmin } from "../controllers/adminControllers.js";
+import { createBook, getallBooks } from "../controllers/bookController.js";
 
 
 const router = Router()
 
-router.route('/registeradmin').post(createAdmin)
-router.route('/blockadmin').put(triggerblockadmin)
-router.route('/getalladmins').get(getallAdmins)
-router.route('/getaadmin').get(getaAdmin)
-router.route('/updateadmin').put(updateAdmin)
+router.route('/registeradmin').post(createAdmin);
+router.route('/blockadmin').put(triggerblockadmin);
+router.route('/getalladmins').get(getallAdmins);
+router.route('/getaadmin').get(getaAdmin);
+router.route('/updateadmin').put(updateAdmin);
 router.route('/getIssues').get(getallIssues);
+router.route('/createBook').post(createBook);
+router.route('/getallBooks').get(getallBooks);
+
 
 
 export default router
