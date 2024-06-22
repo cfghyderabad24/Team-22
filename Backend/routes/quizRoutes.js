901d5225bd84687d8dbccb5e2ead60f5e99d415e
Suraj Kumar <surajkumar.ayanokoji.7@gmail.com>
2024-06-23 03:37:29 +0530

    Added json2csv Package

--- Backend/routes/quizRoutes.js
+++ Backend/routes/quizRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { createQuestions, getQuestions, userQuestions } from '../controllers/quizController.js';
+import { createQuestions, getBookNames, getQuestions, userQuestions } from '../controllers/quizController.js';
 
 const router = express.Router();
 
 router.route('/create').post(createQuestions);
 router.route('/questions/:bookName').get(getQuestions);
 router.route('/submitquiz').post(userQuestions);
+router.route('/books').get(getBookNames);
 
 export default router;
