6b9bce68abab3f388ffb0408eb63d82922bdfa74
yaswanthvitta <yaswanthvitta@gmail.com>
2024-06-22 19:16:07 +0530

    add charts

--- room-to-read/src/App.js
+++ room-to-read/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import StudentProfile from './components/Student/student';
+
+
+
 
 function App() {
   return (
--- room-to-read/src/components/Student/student.js
+++ room-to-read/src/components/Student/student.js
@@ -1,6 +1,7 @@
-
-
-
+import StudentPerformence1 from "../StudentPerformence/StudentPerformence1";
+import StudentPerformence2 from "../StudentPerformence/StudentPerformence2";
+import StudentPerformence3 from "../StudentPerformence/StudentPerformence3";
+import EditStudentInfo from "./editInfo";
 
 
 const StudentProfile = () =>{
@@ -9,36 +10,43 @@ const StudentProfile = () =>{
         <>
         <div>
         <div>
+        </div>
+        <div>
+            <div className=" w-[400px] block p-6 bg-white border border-gray-200 rounded-lg shadow  dark:bg-gray-800 dark:border-gray-700 m-3">
+                <div className="flex flex-row">
+                    <label className="m-2 font-semibold">Name:</label>
+                    <p className="m-2">Student name</p>
+                </div>
+                
+                <div className="flex flex-row">
+                    <label className="m-2 font-semibold">Class:</label>
+                    <p className="m-2">Class VI</p>
+                </div>
 
+                <div className="flex flex-row">
+                    <label className="m-2 font-semibold">School Name:</label>
+                    <p className="m-2">Governament High School</p>
+                </div>
+
+                <div className="flex flex-row">
+                    <label className="m-2 font-semibold">Number of books Taken:</label>
+                    <p className="m-2">2 books</p>
+                </div>
+                <div className="flex flex-row justify-end">
+                    <EditStudentInfo/>
+                </div>
+            </div>
+        </div>
+        </div>
+        <div className="flex flex-row justify-center">
+           <StudentPerformence1/>
+           <StudentPerformence2/>
+        </div>
+        <div className="flex flex-row justify-center">
+           <StudentPerformence3/>
+           <StudentPerformence2/>
         </div>
         
-        <div>
-            <div className="flex flex-row">
-                <label>Name:</label>
-                <p>Student name</p>
-            </div>
-
-            <div>
-                <lable>Class</lable>
-                <p>Class VI</p>
-            </div>
-
-            <div>
-                <lable>School Name</lable>
-                <p>Governament High School</p>
-            </div>
-
-            <div>
-                <label>Location</label>
-                <p>Andhra predesh</p>
-            </div>
-            <div>
-                <label>Number of Books taken</label>
-                <p>{2} + Books</p>
-            </div>
-
-        </div>
-        </div>
         </>
     )
 }
