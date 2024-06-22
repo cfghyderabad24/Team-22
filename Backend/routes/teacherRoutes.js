9e0c44d222235a285ed67478fc3e55fe39b4f3a8
Karthik Ch <saipavan39dh@gmail.com>
2024-06-23 00:07:58 +0530

    added teacher Routes and authMiddlewareteacher

--- Backend/routes/teacherRoutes.js
+++ Backend/routes/teacherRoutes.js
@@ -1,15 +1,16 @@
 import { Router } from "express";
 import { createTeacher, getaTeacher, getallTeachers, loginTeacher, triggerblockteacher, updateTeacher } from "../controllers/teacherController.js";
 
+import { authMiddleWareAdmin, authMiddleWareTeacher } from "../middleware/roleManage.js";
 
 
 const router = Router()
 
 router.route('/registerteacher').post(createTeacher)
-router.route('/blockteacher').put(triggerblockteacher)
-router.route('/getallteachers').get(getallTeachers)
-router.route('/getateacher').get(getaTeacher)
-router.route('/updateteacher').put(updateTeacher)
+router.route('/blockteacher').put(authMiddleWareAdmin,triggerblockteacher)
+router.route('/getallteachers').get(authMiddleWareTeacher,getallTeachers)
+router.route('/getateacher').get(authMiddleWareTeacher,getaTeacher)
+router.route('/updateteacher').put(authMiddleWareTeacher,updateTeacher)
 router.route('/loginTeacher').get(loginTeacher)
 
 
