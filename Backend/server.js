1d619c5181cb41db61506a3fa94a3462f0804b1b
Karthik Ch <saipavan39dh@gmail.com>
2024-06-22 13:13:59 +0530

    Added env file

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,14 @@
 import express from 'express'
 import dotenv from 'dotenv/config'
 import cors from 'cors'
+
+const app = express()
+const PORT = process.env.PORT || 8080
+
+app.use(cors())
+app.use(express.json())
+app.use(morgan('dev'))
+
+app.use(cookieParser())
+
+
