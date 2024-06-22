e59c409ff1993bc85af7b8774b2cad6192c543d6
Suraj Kumar <surajkumar.ayanokoji.7@gmail.com>
2024-06-22 20:31:40 +0530

    Made some changes in Book and Google Module

--- Backend/models/google.js
+++ Backend/models/google.js
@@ -4,7 +4,7 @@ const GoogleSchema = new mongoose.Schema({
     googleId: {type:String, required:true},
     name: {type:String, required:true},
     email:{type:String, required:true},
-});
+},{timestamps:true});
 
 const Google = mongoose.model('Google',GoogleSchema);
 
