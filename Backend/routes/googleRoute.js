da83ec8cab7ccd749143c598eb259e5e9c1c088d
Suraj Kumar <surajkumar.ayanokoji.7@gmail.com>
2024-06-22 16:14:25 +0530

    Callback Initialized

--- Backend/routes/googleRoute.js
+++ Backend/routes/googleRoute.js
@@ -1,15 +1,31 @@
 import express from 'express';
-import passport form 'passport';
 import jwt from 'jsonwebtoken';
-import bcrypt form 'bcryptjs';
+import passport from 'passport';
 import dotenv from 'dotenv';
-import middleware from '../middleware/auth.js';
 import initializePassport from '../services/passport.js';
+
+dotenv.config();
 
 initializePassport(passport);
 const router = express.Router();
 
 router.get('/google',passport.authenticate('google',{scope:['profile','email']}));
+
+router.get('/google/callback', passport.authenticate('google',{session:false}), async(request,response) => {
+    const payload = {
+        user : {
+            id: request.user.id,
+            name: request.user.name,
+            email: request.user.email
+        }
+    };
+
+    jwt.sign(payload,jwtSecret, {expiresIn:'1h'}, (err,token) => {
+        if(err) throw err;
+        response.cookie('token',token,{httpOnly:true});
+        response.redirect('http://localhost:3000/home');
+    });
+});
 
 
 
