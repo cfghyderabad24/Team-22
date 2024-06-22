e26b9762d10595f1f04f14f16390f4293b125b99
yaswanthvitta <yaswanthvitta@gmail.com>
2024-06-22 15:53:03 +0530

    tailwind,headlesui

---
+++ room-to-read/src/components/Student/student.js
@@ -0,0 +1,47 @@
+
+
+
+
+
+const StudentProfile = () =>{
+
+    return(
+        <>
+        <div>
+        <div>
+
+        </div>
+        
+        <div>
+            <div className="flex flex-row">
+                <label>Name:</label>
+                <p>Student name</p>
+            </div>
+
+            <div>
+                <lable>Class</lable>
+                <p>Class VI</p>
+            </div>
+
+            <div>
+                <lable>School Name</lable>
+                <p>Governament High School</p>
+            </div>
+
+            <div>
+                <label>Location</label>
+                <p>Andhra predesh</p>
+            </div>
+            <div>
+                <label>Number of Books taken</label>
+                <p>{2} + Books</p>
+            </div>
+
+        </div>
+        </div>
+        </>
+    )
+}
+
+
+export default StudentProfile;
