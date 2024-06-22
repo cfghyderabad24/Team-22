90d6114e96cc8ccb2ce759552c3614bbac5135a4
Suraj Kumar <surajkumar.ayanokoji.7@gmail.com>
2024-06-23 02:20:38 +0530

    Completed a Quiz Controller

--- Backend/controllers/quizController.js
+++ Backend/controllers/quizController.js
@@ -11,3 +11,26 @@ export const getQuestions = asyncHandler(async(req,res)=> {
         res.status(500).json({ message: err.message });
     }
 });
+
+// Submit user attempt
+export const userQuestions = asyncHandler(async(req,res)=> {
+    const {userId, answers} = req.body;
+    try {
+        const ques = await question.find();
+        let score = 0;
+        ques.forEach((q,index) => {
+            if(answers[index] === question.correct) {
+                score++;
+            }
+        });
+        const ua = new userAttempt({
+            userId,
+            answers,
+            score
+        });
+        await ua.save();
+        res.json({score, totalQuestions: getQuestions.length});
+    } catch (err) {
+        res.status(500).json({message: err.message});
+    }
+});
