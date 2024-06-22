32deecafc3a37df0b8208b7965eb82a0bdfc5a13
Karthik Ch <saipavan39dh@gmail.com>
2024-06-22 18:26:38 +0530

    student routes completed

--- Backend/routes/studentRoutes.js
+++ Backend/routes/studentRoutes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { createStudent } from "../controllers/studentControllers.js";
+import { createStudent, deleteStudent, getaStudent, getallStudents } from "../controllers/studentControllers.js";
 
 const router = Router()
 
-router.route('/register').post(createStudent)
+router.route('/registerstudent').post(createStudent)
+router.route('/deletestudent').delete(deleteStudent)
+router.route('/getallstudents').put(getallStudents)
+router.route('/getastudent').put(getaStudent)
+
 
 export default router
