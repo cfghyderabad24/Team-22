45a81a16989aa5202fe695787b28cebe7ee0bd29
Suraj Kumar <surajkumar.ayanokoji.7@gmail.com>
2024-06-23 06:14:04 +0530

    Converted Number

--- Backend/models/checkin.js
+++ Backend/models/checkin.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const checkinSchema = new mongoose.Schema({
     student_id: { type: String, required: true },
     book_id: { type: String, required: true },
-    book_level: { type: String, required: true },
+    book_level: { type: Number, required: true },
     genuine: { type: Boolean, required: true },
     timestamp: { type: Date, default: Date.now }
 });
