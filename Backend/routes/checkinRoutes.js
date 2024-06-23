54bf0e2139500c72c58ef3e747289d56ae276a4d
Suraj Kumar <surajkumar.ayanokoji.7@gmail.com>
2024-06-23 05:58:08 +0530

    Added Checkin Routes

--- Backend/routes/checkinRoutes.js
+++ Backend/routes/checkinRoutes.js
@@ -1,1 +1,11 @@
+import express from 'express';
+import { checkinStudent, checkoutStudent, getCheckins } from './controllers.js';
+
+const router = express.Router();
+
+router.route('/checkin').post(checkinStudent);
+router.route('/checkout').post(checkoutStudent);
+router.route('/checkins').get(getCheckins);
+
+export default router;
  
